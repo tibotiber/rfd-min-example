5a87954d73dac73f5b94524c47835aca209795c8
Oliver Caldwell <olliec87@gmail.com>
2017-06-02 12:28:24 +0100

    Update minimal example
    Works with:
    https://github.com/Olical/react-faux-dom/pull/102#issuecomment-305759850

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import * as d3 from 'd3'
 import {withFauxDOM} from 'react-faux-dom'
 
 class MyReactComponent extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      chart: 'loading...'
-    }
-  }
-
   componentDidMount () {
     const faux = this.props.connectFauxDOM('div', 'chart')
     d3.select(faux)
@@ -23,11 +16,15 @@ class MyReactComponent extends React.Component {
       <div>
         <h2>Here is some fancy data:</h2>
         <div className='renderedD3'>
-          {this.state.chart}
+          {this.props.chart}
         </div>
       </div>
     )
   }
+}
+
+MyReactComponent.defaultProps = {
+  chart: 'loading'
 }
 
 export default withFauxDOM(MyReactComponent)
